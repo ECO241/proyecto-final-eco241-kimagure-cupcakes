2b8402ea8b94b3da6632e4e1bbefa28d046c7078
Dan <daramirezp1112@gmail.com>
2024-04-22 21:40:09 -0500

    flavors stuff

--- src/routes/flavorsRoute.js
+++ src/routes/flavorsRoute.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const flavorsController = require('../controllers/flavorsController');
 
-router.get('/', (req, res) => { /*falta referenciar controllers y services*/});
+router.get('/', flavorsController.getAllFlavors);
+
+router.get('/:id', flavorsController.getFlavorById);
+
+router.post('/', flavorsController.createFlavor);
+
+router.put('/:id', flavorsController.updateFlavor);
+
+router.delete('/:id', flavorsController.deleteFlavor);
 
 module.exports = router;
