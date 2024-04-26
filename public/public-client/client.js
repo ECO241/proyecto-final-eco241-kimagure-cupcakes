99077822f0d49e8d690130066d95962e9c4ceb3f
Natjimor <jimenezorozconatalia@gmail.com>
2024-04-26 00:10:34 -0500

    intento de crear usuario

---
+++ public/public-client/client.js
@@ -0,0 +1,71 @@
+const socket = io();
+
+const start = document.querySelector('.start');
+const namediv = document.querySelector('.name');
+const options = document.querySelector('.options');
+const qr = document.querySelector('.qr');
+
+const startbtn = document.querySelector('.startbtn');
+const nameinput = document.querySelector('.nameinput');
+const namebtn = document.querySelector('.namebtn');
+
+//opciones
+const optionsList = [
+    ['Cake','vanilla', 'chocolate', 'strawberry', 'banana'],
+    ['Icing', 'apple', 'banana', 'orange', 'grape'],
+    ['Toppings', 'sprinkles', 'chocolate syrup', 'whipped cream', 'cherry']
+];
+
+let cupcake = {
+    name: 'Pending...',
+    flavor: 'Pending...',
+    icing: 'Pending...',
+    topping: 'Pending...'
+}
+
+startbtn.addEventListener('click', () => {
+    start.style.display = 'none';
+    namediv.style.display = 'block';
+})
+
+namebtn.addEventListener('click', () => {    
+    cupcake.name = nameinput.value;
+    socket.emit('update', cupcake);
+
+    namediv.style.display = 'none';
+    options.style.display = 'block';
+    displayOptions();
+})
+
+function displayOptions() {
+     // Agrega las opciones a cada elemento 
+    optionsList.forEach((list, i) => {
+        const option = document.createElement('select');
+        option.classList.add('option');
+        option.addEventListener('change', () => {
+            // Cuando se selecciona una opción, actualiza la propiedad correspondiente de cupcake
+            cupcake[Object.keys(cupcake)[i+1]] = option.value;
+            //Emite un evento de la actualizacion del cucake
+            socket.emit('update', cupcake);
+        })
+        list.forEach((item) => {
+            const opt = document.createElement('option');
+            opt.value = item;
+            opt.textContent = item;
+            option.appendChild(opt);
+        })
+        options.appendChild(option);
+    })
+    const finishbtn = document.createElement('button');
+    finishbtn.textContent = 'Finish';
+    options.appendChild(finishbtn);
+    finishbtn.addEventListener('click', () => {
+        options.style.display = 'none';
+        // Cuando se hace clic en el botón, oculta las opciones y muestra el código QR
+        qr.style.display = 'block';
+        // Emite un evento de "finalizar pedido" al servidor a través del socket junto con la información del cupcake
+        socket.emit('finishorder', cupcake);
+    })
+}
+ 
+
---
+++ src/services/flavorsService.js
@@ -0,0 +1,68 @@
+const {createClient} = require('@supabase/supabase-js');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_TEAM;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+    console.error("Supabase URL or key is missing. Make sure to set SUPABASE_TEAM and SUPABASE_KEY environment variables.");
+    process.exit(1);
+}
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+const tablename = " ";
+
+const flavorsService = {
+    getAllFlavors: async () => {
+        const { data, error } = await supabase
+            .from(tablename)
+            .select();
+        if (error) {
+            throw new Error(error.message);
+        }
+        return data;
+    },
+
+    getFlavorById: async (id) => {
+        const { data, error } = await supabase
+            .from(tablename)
+            .select()
+            .eq('id', id);
+        if (error) {
+            throw new Error(error.message);
+        }
+        return data;
+    },
+
+    createFlavor: async (flavor) => {
+        const { error } = await supabase
+            .from(tablename)
+            .insert(flavor);
+        if (error) {
+            throw new Error(error.message);
+        }
+    },
+
+    updateFlavor: async (id, flavor) => {
+        const { error } = await supabase
+            .from(tablename)
+            .update(flavor)
+            .eq('id', id);
+        if (error) {
+            throw new Error(error.message);
+        }
+    },
+
+    deleteFlavor: async (id) => {
+        const { error } = await supabase
+            .from(tablename)
+            .delete()
+            .eq('id', id);
+        if (error) {
+            throw new Error(error.message);
+        }
+    },
+};
+
+module.exports = flavorsService;
